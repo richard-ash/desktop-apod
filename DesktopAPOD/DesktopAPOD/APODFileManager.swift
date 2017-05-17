b67dbadb0670e67504e64287ce31e66abf99cf22
Richard <r.ash8347@gmail.com>
2017-05-17 13:15:09 -0700

    - Fixed bugs in APODFileManager - Updated entitlements - Updated the dmg

--- DesktopAPOD/DesktopAPOD/APODFileManager.swift
+++ DesktopAPOD/DesktopAPOD/APODFileManager.swift
@@ -11,22 +11,38 @@ import Cocoa
 
 class APODFileManager {
   
-  // MARK: - Variables
+  // MARK: - Objects
+  
+  enum FileError: Error {
+    case couldNotFindMainScreen
+    case couldNotParseImage
+  }
+  
+  // MARK: - Properties
   
   lazy var apodImageURL: URL = {
     return self.apodDirectoryURL.appendingPathComponent("apodImage.png")
   }()
   
-  // MARK: - Private Functions
+  // MARK: - Private Properties
   
-  private let fileManager = FileManager.default
-  private let apodDirectoryURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".APODImages", isDirectory: true)
+  private let fileManager: FileManager
+  
+  private lazy var apodDirectoryURL: URL = {
+    return self.fileManager.urls(for: .downloadsDirectory, in: .userDomainMask).first!.appendingPathComponent(".apodImages", isDirectory: true)
+  }()
+  
+  // MARK: - Initialization
+  
+  init(fileManager: FileManager = FileManager.default) {
+    self.fileManager = fileManager
+  }
   
   // MARK: - Functions
   
   func createAPODDirectory() {
     do {
-      try FileManager.default.createDirectory(at: apodDirectoryURL, withIntermediateDirectories: false, attributes: nil)
+      try fileManager.createDirectory(at: apodDirectoryURL, withIntermediateDirectories: false, attributes: nil)
     } catch {
       NSLog("Failed to create directory: \(error)")
     }
@@ -41,14 +57,14 @@ class APODFileManager {
   }
   
   func saveAPODImage(_ image: NSImage) throws {
-    guard let tiffData = image.tiffRepresentation else { return }
+    guard let tiffData = image.tiffRepresentation else { throw FileError.couldNotParseImage }
     let imageRep = NSBitmapImageRep(data: tiffData)
-    guard let imageData = imageRep?.representation(using: .PNG, properties: [:]) else { return }
+    guard let imageData = imageRep?.representation(using: .PNG, properties: [:]) else { throw FileError.couldNotParseImage }
     try imageData.write(to: apodImageURL)
   }
   
   func updateDesktopImageWithSavedAPOD() throws {
-    guard let mainScreen = NSScreen.screens()?.first else { return }
+    guard let mainScreen = NSScreen.screens()?.first else { throw FileError.couldNotFindMainScreen }
     try NSWorkspace.shared().setDesktopImageURL(apodImageURL, for: mainScreen, options: [:])
   }
 }
